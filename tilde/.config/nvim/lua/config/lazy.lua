1f287faaf722fb4e658a4924384371f16ce517e0
Alexandru Bereghici <abereghici@users.noreply.github.com>
2024-08-24 14:18:19 +0300

    Enable neotest

--- tilde/.config/nvim/lua/config/lazy.lua
+++ tilde/.config/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.test.core" },
 
     -- import/override with your plugins
     { import = "plugins" },
